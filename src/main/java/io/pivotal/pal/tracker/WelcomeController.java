0691a0cf07b231c046149b13b102c19b12db74f3
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:04 -0500

    lab 2 - configuration with env variables

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    @Value("${welcome_message}")
+    private String helloText;
+
+    public WelcomeController(@Value("${welcome_message}") String helloText) {
+        this.helloText = helloText;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+
+        return helloText;
+    }
+
 }
